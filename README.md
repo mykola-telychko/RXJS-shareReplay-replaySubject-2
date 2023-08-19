a113ae945b0e0287a7e9ae6b21be9eefcf743d35
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 22:25:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-shareReplay-replaySubject-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-24mwyu)
---
+++ index.ts
@@ -0,0 +1,21 @@
+import { Subject, ReplaySubject } from 'rxjs';
+import { pluck, share, shareReplay, tap } from 'rxjs/operators';
+
+// https://www.learnrxjs.io/learn-rxjs/operators/multicasting/sharereplay
+
+// simulate url change with subject
+const routeEnd = new Subject<{data: any, url: string}>();
+// grab url and share with subscribers
+const lastUrl = routeEnd.pipe(
+  tap(_ => console.log('executed')),
+  pluck('url'),
+  // defaults to last 1 value
+  shareReplay()
+);
+// requires initial subscription
+const initialSubscriber = lastUrl.subscribe(console.log)
+// simulate route change
+// logged: 'executed', 'my-path'
+routeEnd.next({data: {}, url: 'my-path'});
+// logged: 'my-path'
+const lateSubscriber = lastUrl.subscribe(console.log);
